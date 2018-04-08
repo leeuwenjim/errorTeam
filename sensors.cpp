684de59886245b2280145df8e166693408618f79
ChristyBrouwer <christy.brouwer@student.hu.nl>
2018-04-08 11:47:52 +0200

    Added doxygen comment style
    /// For main comments

--- sensors.cpp
+++ sensors.cpp
@@ -1,15 +1,17 @@
-#include "BrickPi3.h" //for BrickPi3, this file sets up the BrickPi for usage
-#include <iostream> //for cout
-#include <unistd.h> //for sleep
-#include <signal.h> //for exit signals
-#include <iomanip> //for setw and setprecision
+#include "BrickPi3.h" /// For BrickPi3, this file sets up the BrickPi for usage
+#include <iostream> /// For cout
+#include <unistd.h> /// For sleep
+#include <signal.h> /// For exit signals
+#include <iomanip> /// For setw and setprecision
 using namespace std;
 
 BrickPi3 BP;
 
 void exit_signal_handler(int signo);
 
-//Setting up sensors as functions so they can be reused
+/// Setting up sensors as functions so they can be reused
+
+/// Setup ultrasonic reading
 int ultrasonic(){
 	BP.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_ULTRASONIC);
 	sensor_ultrasonic_t Ultrasonic;
@@ -19,6 +21,7 @@ int ultrasonic(){
 	return Ultrasonic.cm;
 }
 
+/// Setup left sensor reading
 int leftBW(){
 	BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_LIGHT_ON);
 	sensor_light_t Blacknwhitelinks;
@@ -31,6 +34,7 @@ int leftBW(){
 	return Blacknwhitelinks.reflected;
 }
 
+/// Setup right sensor reading
 int rightBW(){
 	BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_LIGHT_ON);
 	sensor_light_t Blacknwhiterechts;
@@ -43,14 +47,15 @@ int rightBW(){
 	return BlacknWhiterechts.reflected;
 }
 
+/// Main function is used to check all sensors, f.e. for testing
 int main(){
-	signal(SIGINT, exit_signal_handler); //exit function for ctrl c
+	signal(SIGINT, exit_signal_handler); /// Exit function for ctrl c
 	
-	BP.detect(); //make sure Pi is communicating and up to date
+	BP.detect(); /// Make sure Pi is communicating and up to date
 	
 	int error;
 	
-	while (true){ //check sensors
+	while (true){ /// Check sensors
 		error = 0;
 		ultrasonic();
 		linksZW();
@@ -58,7 +63,7 @@ int main(){
 	}
 }
 
-//Signal handler when Ctrl-C is pressed, makes sure nothing stays running
+/// Signal handler when Ctrl-C is pressed, makes sure nothing stays running
 void exit_signal_handler(int signo){
 	if(signo == SIGINT){
 		BP.reset_all();
