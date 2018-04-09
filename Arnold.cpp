7496968101075092ea46c1cb0afdb7b363f6ea9e
Levi Bravenboer <36155755+LeviBravenboer@users.noreply.github.com>
2018-04-09 14:46:46 +0200

    Update Arnold.cpp

--- Arnold.cpp
+++ Arnold.cpp
@@ -46,13 +46,13 @@ void Arnold::move(uint16_t powerLeft, uint16_t powerRight) {
 
 void Arnold::turn_ultrasonic(int position){
 	if(position ==1){
-		BP.set_motor_position(Arnold::motortop,0)
+		BP.set_motor_position(Arnold::motortop,0);
 	}
 	elif(position ==2){
-		BP.set_motor_position(Arnold::motortop ,90)
+		BP.set_motor_position(Arnold::motortop ,90);
 	}
 	elif(position == 3){
-		BP.set_motor_position(Arnold::motortop ,180)
+		BP.set_motor_position(Arnold::motortop ,180);
 	}
 }
 
