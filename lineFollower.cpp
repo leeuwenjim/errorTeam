26f80e07bcf51db88c064cc9475456176deeda18
Jim van Leeuwen <jim.vanleeuwen@student.hu.nl>
2018-04-09 20:00:05 +0200

    first try at line following

--- lineFollower.cpp
+++ lineFollower.cpp
@@ -8,9 +8,34 @@ Arnold arnold;
 int main() {
 
 	arnold.calibrate();
+	int maxPower = 30;
+	int currentLeftPower = 15;
+	int currentRightPower = 15;
+	
+	int increments = 5;
 	
 	while(true) {
-		cout << arnold.leftSideOnLine()	<< endl;
+		
+		if (arnold.leftSideOnLine()) {
+			if (currentLeftPower > 0) {
+				currentLeftPower -= increments;
+			}
+		} else {
+			if (currentLeftPower < maxPower) {
+				currentLeftPower += increments;
+			}
+		}
+		
+		if (arnold.rightSideOnLine()) {
+			if (currentRightPower > 0) {
+				currentRightPower -= increments;
+			}
+		} else {
+			if (currentRightPower < maxPower) {
+				currentRightPower += increments;
+			}
+		}
+		
 	}
 	
 }
