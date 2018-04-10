df3794be8e282e857b16599c12507bc6efc7d99d
leeuwenjim <31850503+leeuwenjim@users.noreply.github.com>
2018-04-10 11:22:56 +0200

    Update ArnoldClassTester.cpp

--- ArnoldClassTester.cpp
+++ ArnoldClassTester.cpp
@@ -12,12 +12,6 @@ int main() {
 		arnold.getRightBW();
 		arnold.turn_ultrasonic(2);
 		
-		arnold.move(30, 30);
-		sleep(1);
-		arnold.stop();
-		arnold.move(60, 0);
-		sleep(2);
-		arnold.stop();
 		
 	}
 		
