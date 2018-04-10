90612c47c450b9064e120ff61b51c1030833553c
Jim van Leeuwen <jim.vanleeuwen@student.hu.nl>
2018-04-10 13:30:54 +0200

    bug fix

--- navTest.cpp
+++ navTest.cpp
@@ -8,10 +8,10 @@ Arnold arnold;
 bool once = false;
 
 vector<int> directions = {1, 0, 0, 2, 2, 0, 0, 1, 0, 1, 1, 0};
-int index = 0;
 
 int main() {
 	arnold.calibrate();
+	int index = 0;
 	while (true) {
 		if (arnold.leftSideOnLine() && arnold.rightSideOnLine()) {
 			arnold.crossNavigator(directions[index]);
