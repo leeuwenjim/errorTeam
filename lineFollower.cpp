7523df0606f749c8f8f6f335ca73e2b872eec762
Jim van Leeuwen <jim.vanleeuwen@student.hu.nl>
2018-04-10 12:09:13 +0200

    example

--- lineFollower.cpp
+++ lineFollower.cpp
@@ -11,7 +11,12 @@ int main() {
 	arnold.calibrate();
 	
 	while(true) {
-		arnold.lineFollowAlgoritm();
+		//if (arnold.seesomething) {
+				//arnold.goAroundObstacle
+				//get back on line
+		//} else 
+			arnold.lineFollowAlgoritm();
+		//}
 	}
 	
 }
