61dc3db2d703c658f7c17819bd3e2b6ff9518e79
leeuwenjim <31850503+leeuwenjim@users.noreply.github.com>
2018-04-10 14:39:04 +0200

    Update Arnold.h

--- Arnold.h
+++ Arnold.h
@@ -5,37 +5,37 @@
 
 class Arnold {
 private:
-	uint8_t motorleft;
-	uint8_t motorright;
-	uint16_t BWsensorleft;
-	uint16_t BWsensorright;
-	uint8_t motortop;
-	uint8_t ultrasonic;
-	int leftWhiteValue;
-	int rightWhiteValue;
-	int BWMargin = 150;
-	int maxLineFollowPower = 40;
-	int minLineFollowPower = -15;
-	int currentLeftPower = 15;
-	int currentRightPower = 15;
+	uint8_t motorleft; ///< left motor port
+	uint8_t motorright; ///< right motor port
+	uint16_t BWsensorleft; ///< left reflection sensor port
+	uint16_t BWsensorright; ///< right reflection sensor port
+	uint8_t motortop; ///< ultrasonic motor port
+	uint8_t ultrasonic; ///< ultrasonic sensor port
+	int leftWhiteValue; ///< calibrated white value on the left side
+	int rightWhiteValue; ///< calibrated white value on the right side
+	int BWMargin = 150; ///< margin used during line following, can be seen as sensitivity
+	int maxLineFollowPower = 40; ///< line follow maximum speed
+	int minLineFollowPower = -15; ///< line follow minimum speed
+	int currentLeftPower = 15; ///< power of the left motor during line following
+	int currentRightPower = 15; ///< power of the right motor during line following
 public:
-	Arnold();
-	void move(uint16_t powerLeft, uint16_t powerRight);
-	void stop();
-	void turn_ultrasonic(int position);
-	int getUltrasonic();
-	int getLeftBW();
-	int getRightBW();
-	int getLeftWhiteValue();
-	int getRightWhiteValue();
-	void calibrate();
-	bool leftSideOnLine();
-	bool rightSideOnLine();
-	int setBWMargin(uint16_t margin);
-	void lineFollowAlgoritm();
-	void crossNavigator(int direction);
-	void turn(int side);
-	void goAroundObstacle();
+	Arnold(); ///< constructor
+	void move(uint16_t powerLeft, uint16_t powerRight); ///< set motor speeds
+	void stop(); ///< stop motors
+	void turn_ultrasonic(int position); ///< turn ultrasonic motor
+	int getUltrasonic(); ///< get value from ultrasonic
+	int getLeftBW(); ///< get value from left reflection sensor
+	int getRightBW(); ///< get value from right reflection sensor
+	int getLeftWhiteValue(); ///< get white calibrated value on the left side
+	int getRightWhiteValue(); ///< get white calibrated value on the right side
+	void calibrate(); ///< calibrate arnold
+	bool leftSideOnLine(); ///< check if left side is on the line
+	bool rightSideOnLine(); ///< check if right side is on the line
+	int setBWMargin(uint16_t margin); ///< set margin for line following
+	void lineFollowAlgoritm(); ///< line following function, only a single iteration
+	void crossNavigator(int direction); ///< turn left, right, or ahead on a crossing (doesn't detect crossings)
+	void turn(int side); ///< turns arnold 90 degrees
+	void goAroundObstacle(); ///< function that goes around an obstacle, and gets back on the line
 };
 
 #endif
