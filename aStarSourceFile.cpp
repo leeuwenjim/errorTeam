4ac1ec504afe5d6bbaf4df1b4cd0b9f884887248
ThePatman3 <patrick.vanderbend@student.hu.nl>
2018-04-10 13:19:04 +0200

    added all AStar files
    AStar is used to calculate a route from a node/cell to destination node/cell in a grid

---
+++ aStarSourceFile.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include "aStarSourceFile.h"
+using namespace std;
+
+bool operator==(const Coordinate & lhs, const Coordinate & rhs){
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+Node::Node(){
+    this -> blocked = false;
+    this -> gCost = 0;
+    this -> hCost = 0;
+    this -> fCost = 0;
+}
+
+Node::Node(bool _blocked, unsigned int _xCoordinate, unsigned int _yCoordinate){
+    this -> blocked = _blocked;
+    this -> coordinate.x = _xCoordinate;
+    this -> coordinate.y = _yCoordinate;
+    this -> gCost = 0;
+    this -> hCost = 0;
+    this -> fCost = 0;
+}
+
+void Node::updateFCost(){
+    this -> fCost = this -> gCost + this -> hCost;
+}
