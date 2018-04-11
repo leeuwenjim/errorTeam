93a8d727e1561fe1b9195c445b29523081ea333b
leeuwenjim <31850503+leeuwenjim@users.noreply.github.com>
2018-04-11 12:05:50 +0200

    Update Arnold.h

--- Arnold.h
+++ Arnold.h
@@ -35,6 +35,7 @@ public:
 	void lineFollowAlgoritm(); ///< line following function, only a single iteration
 	void crossNavigator(int direction); ///< turn left, right, goes ahead or turns around on a crossing (doesn't detect crossings)
 	void turn(int side); ///< turns arnold 90 degrees
+	void turnDegrees(int degrees);
 	void goAroundObstacle(); ///< function that goes around an obstacle, and gets back on the line
 };
 
