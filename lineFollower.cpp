036316467479d57eb8bb4d966712c30fff6be3ba
Christy Brouwer <31627386+ChristyBrouwer@users.noreply.github.com>
2018-04-11 11:39:02 +0200

    Added doxygen comments

--- lineFollower.cpp
+++ lineFollower.cpp
@@ -7,16 +7,18 @@ using namespace std;
 Arnold arnold;
 
 int main() {
-
+	///Calibrate code to ensure light sensors are set up right.
 	arnold.calibrate();
 	
+	/** While loop will continually check ultrasonic, if it sees an obstacle it'll start obstacle avoid program. Any other moment it
+	* will run the line follow algoritm. */
 	while(true) {
-		if (arnold.getUltrasonic() < 10) {
+		if (arnold.getUltrasonic() < 10) { /// Ensures Arnold won't drive against obstacles
 			arnold.goAroundObstacle();
             cout << "eind object ontwijken" << endl;
 				
 		} 
-		else{ 
+		else{ 	/// If there isn't an object in sight Arnold will follow the line
             cout << "begin lijnvolg" << endl;
 			arnold.lineFollowAlgoritm();
 		}
